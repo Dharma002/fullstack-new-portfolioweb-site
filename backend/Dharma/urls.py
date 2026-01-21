6b769e1c3517f94ae3940b26d61103223a1e582d
dharmrajpatel427@gmail.com <dharmrajpatel426@gmail.com>
2026-01-21 04:31:39 -0800

    fix dharma module path

--- backend/Dharma/urls.py
+++ backend/Dharma/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PortfolioViewSet,ContactCreateView
+from backend.Dharma import views
 
 router = DefaultRouter()
 router.register(r'portfolio', PortfolioViewSet , basename='portfolio')
