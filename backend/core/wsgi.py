cf4729866e4a7e79ac63f44b0efcd7df54473065
dharmrajpatel427@gmail.com <dharmrajpatel426@gmail.com>
2026-01-21 10:25:19 -0800

    Create staticfiles directory for Render

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
+import sys, os
+path = '/opt/render/project/src/backend'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
