d58c924ccaafe4c0632ce54de00e98eb5b94be2b
dharmrajpatel427@gmail.com <dharmrajpatel426@gmail.com>
2026-01-21 03:46:25 -0800

    fixed app paths for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Dharma',
+    'backend.Dharma',
     'corsheaders',# my new app
     'rest_framework',
     
